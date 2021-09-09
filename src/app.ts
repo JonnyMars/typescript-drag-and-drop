a23498344822b2f37b3c02a1b24cb282f9cd5174
Jonny Mars <jonny.mars@iprospect.com>
2021-09-09 20:06:26 +0100

    fix: add bind to event listener

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ class ProjectInput {
     }
 
     private configure() {
-        this.element.addEventListener("submit", this.submitHandler)
+        this.element.addEventListener("submit", this.submitHandler.bind(this))
     }
 
     private attach() {
